635d8ef3168d352ab7f21d5a367ce69d5c7ba510
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-09 20:59:18 +0300

    Create GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,80 @@
+import { createGlobalStyle } from 'styled-components';
+import MulishBlack from '../fonts/Mulish-Black.ttf';
+import MulishBold from '../fonts/Mulish-Bold.ttf';
+import MulishMedium from '../fonts/Mulish-Medium.ttf';
+
+export const GlobalStyles = createGlobalStyle`
+
+@font-face {
+    font-family: "Mulish";
+    src: url(${MulishMedium});
+    font-weight: 500;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: "Mulish";
+    src: url(${MulishBold});
+    font-weight: 700;
+    font-style: normal;
+}
+
+
+@font-face {
+    font-family: "Mulish";
+    src: url(${MulishBlack});
+    font-weight: 900;
+    font-style: normal;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: "Mulish", sans-serif;
+  margin: 0;
+  padding: 0;
+  overflow: scroll;
+}
+
+button {
+  cursor: pointer;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+ol, ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+a {
+    text-decoration: none;
+  }
+
+p {
+  padding: 0;
+  margin: 0;
+}
+
+input, textarea, select, button {
+  font: inherit;
+}
+`;
