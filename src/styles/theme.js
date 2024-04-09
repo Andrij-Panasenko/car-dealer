f1ad70aaf29d9be3b9356029908b3bb62377c9ba
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-09 20:26:36 +0300

    add theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    text: '#1a1a1a',
+    yellow: '#fdb913',
+    blue: '#192459',
+    orange: '#e47f3a',
+    grey: '#eeeeee',
+    subtitle: '#797979',
+  },
+  transition: '300ms cubic-bezier(.13,.5,.73,.89)',
+};
