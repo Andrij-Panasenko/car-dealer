df2df574157f885e17e01c321cd570dd069bb700
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-11 21:41:59 +0300

    commit header + shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Header } from "./components/Header/Header";
+import { SharedLayout } from "./components/SharedLayout/SharedLayout";
+
 export default function App() {
-  return <></>;
+
+  // fetch('http://localhost:3000/api/cars').then((i) => console.log(i));
+  return (
+    <>
+      <SharedLayout>
+          <Header />
+        <main>
+        </main>
+        <footer></footer>
+      </SharedLayout>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter basename='/car-dealer'>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
