48c70245a53e2a74af00d8c7ae0ae824eac6355c
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-11 22:54:36 +0300

    style upper header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,79 +1,116 @@
-import { Link } from 'react-router-dom';
 import sprite from '../../assets/sprite.svg';
+import { Link } from 'react-router-dom';
+
+import {
+  Address,
+  AddressLink,
+  ContactsWrapp,
+  Location,
+  LocationWrapp,
+  Logo,
+  LowerHeader,
+  PageHeader,
+  SocialMediaList,
+  UpperHeader,
+} from './Header.styled';
 
 export const Header = () => {
   return (
     <>
-      <header>
-        <Link to="/">logo</Link>
-        <div>
-          <svg width="24" height="24">
-            <use xlinkHref={sprite + '#map-pin'}></use>
-          </svg>
-          <p>
-            No. 35-37 Huynh Thuc Khang, Ben Nghe Ward, District 1, Ho Chi Minh
-            City
-          </p>
-        </div>
-        <div>
-          <svg width="24" height="24">
-            <use xlinkHref={sprite + '#phone-call'}></use>
-          </svg>
+      <PageHeader>
+        <UpperHeader>
+          <Logo to="/">Car Vip</Logo>
+          <LocationWrapp>
+            <svg width="24" height="24">
+              <use xlinkHref={sprite + '#map-pin'}></use>
+            </svg>
+            <Location>
+              No. 35-37 Huynh Thuc Khang, Ben Nghe Ward, District 1, Ho Chi Minh
+              City
+            </Location>
+          </LocationWrapp>
+          <ContactsWrapp>
+            <svg width="24" height="24">
+              <use xlinkHref={sprite + '#phone-call'}></use>
+            </svg>
 
-          <p>Hotline</p>
-
-          <address>
-            <ul>
-              <li>
-                <Link to="tel:+86-878-9674-4455">+86-878-9674-4455</Link>
-              </li>
-              <li>
-                <Link to="tel:+84-78-2415-33">+84-78-2415-33</Link>
-              </li>
-            </ul>
-          </address>
-        </div>
-        <ul>
-          <li>
-            <svg width="24" height="24">
-              <use xlinkHref={sprite + '#instagram'}></use>
-            </svg>
-          </li>
-          <li>
-            <svg width="24" height="24">
-              <use xlinkHref={sprite + '#facebook'}></use>
-            </svg>
-          </li>
-          <li>
-            <svg width="24" height="24">
-              <use xlinkHref={sprite + '#twitter'}></use>
-            </svg>
-          </li>
-        </ul>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/">New models</Link>
-          <Link href="/">SERVICES</Link>
-          <Link href="/">ABOUT US</Link>
-          <Link href="/">NEWS</Link>
-          <Link href="/">CONTACT</Link>
-        </nav>
-        <button type="button">Contact Seller</button>
-        <ul>
-          <li>
-            <p>Compare</p>
-            <svg width="26" height="26">
-              <use xlinkHref={sprite + '#diagram'}></use>
-            </svg>
-          </li>
-          <li>
-            <p>Cart</p>
-            <svg width="26" height="26">
-              <use xlinkHref={sprite + '#cart'}></use>
-            </svg>
-          </li>
-        </ul>
-      </header>
+            <Address>
+              <ul>
+                <li>
+                  <AddressLink to="tel:+86-878-9674-4455">
+                    +86-878-9674-4455
+                  </AddressLink>
+                </li>
+                <li>
+                  <AddressLink to="tel:+84-78-2415-33">
+                    +84-78-2415-33
+                  </AddressLink>
+                </li>
+              </ul>
+            </Address>
+          </ContactsWrapp>
+          <SocialMediaList>
+            <li>
+              <Link
+                to="https://www.instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg width="24" height="24">
+                  <use xlinkHref={sprite + '#instagram'}></use>
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="https://www.facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg width="24" height="24">
+                  <use xlinkHref={sprite + '#facebook'}></use>
+                </svg>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg width="24" height="24">
+                  <use xlinkHref={sprite + '#twitter'}></use>
+                </svg>
+              </Link>
+            </li>
+          </SocialMediaList>
+        </UpperHeader>
+        <LowerHeader>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/">New models</Link>
+            <Link href="/">SERVICES</Link>
+            <Link href="/">ABOUT US</Link>
+            <Link href="/">NEWS</Link>
+            <Link href="/">CONTACT</Link>
+          </nav>
+          <button type="button">Contact Seller</button>
+          <ul>
+            <li>
+              <p>Compare</p>
+              <svg width="26" height="26">
+                <use xlinkHref={sprite + '#diagram'}></use>
+              </svg>
+            </li>
+            <li>
+              <p>Cart</p>
+              <svg width="26" height="26">
+                <use xlinkHref={sprite + '#cart'}></use>
+              </svg>
+            </li>
+          </ul>
+        </LowerHeader>
+      </PageHeader>
     </>
   );
 };
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const PageHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  background-color: transparent;
+`;
+
+export const Logo = styled(Link)`
+  font-weight: 900;
+  font-size: 30px;
+  letter-spacing: 0.05em;
+  text-transform: capitalize;
+
+  color: ${(p) => p.theme.colors.blue};
+`;
+
+export const UpperHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  outline: 1px solid red;
+`;
+
+export const LocationWrapp = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+
+export const Location = styled.p`
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: 0.05em;
+  width: 100%;
+  max-width: 339px;
+
+  color: ${(p) => p.theme.colors.text};
+`;
+
+export const ContactsWrapp = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  position: relative;
+`;
+
+export const Address = styled.address`
+  &::before {
+    content: 'Hotline:';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 12px;
+    letter-spacing: 0.05em;
+    position: absolute;
+    top: -15px;
+
+    color: ${(p) => p.theme.colors.orange};
+  }
+`;
+
+export const AddressLink = styled(Link)`
+  font-weight: 900;
+  font-size: 14px;
+  letter-spacing: 0.05em;
+  font-style: normal;
+
+  color: ${(p) => p.theme.colors.blue};
+`;
+
+export const SocialMediaList = styled.ul`
+  display: flex;
+  gap: 16px;
+
+  & > li {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const LowerHeader = styled.div`
+  display: flex;
+  outline: 1px solid red;
+`;
