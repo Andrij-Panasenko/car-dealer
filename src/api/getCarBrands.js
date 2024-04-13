8e8f1e0f5d555462069098a2d38b451e1e6cfff6
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-14 00:10:30 +0300

    upd base URL

--- src/api/getCarBrands.js
+++ src/api/getCarBrands.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-axios.defaults.baseURL = 'https://car-dealer-webserver.onrender.com';
+axios.defaults.baseURL = 'https://car-dealer-webserver.onrender.com/api';
 
 export const getCarBrands = async () => { 
     const response = await axios.get("/carbrands");
 
-    return response;
+    return response.data;
 }
