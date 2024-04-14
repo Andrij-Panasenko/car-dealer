ed6f1bac00cffbb0e330856b020af9dffeb4605c
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-14 15:16:56 +0300

    replaced GlobalStyles to index.css
    + add modern-normalize

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
 import { SearchForm } from './components/SearchForm/SearchForm';
 import { SharedLayout } from './components/SharedLayout/SharedLayout';
+import "./index.css"
 
 export default function App() {
   // fetch('http://localhost:3000/api/cars').then((i) => console.log(i));
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,8 @@
-import { GlobalStyles } from '../../styles/GlobalStyles';
 import { Layout } from './SharedLayout.styled';
 export const SharedLayout = ({ children }) => {
   return (
     <>
       <Layout>{children}</Layout>
-      <GlobalStyles />
     </>
   );
 };
