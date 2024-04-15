8ccc9891cfb3c49e08ce152ea05c6fcd7bb0c4f0
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-15 23:22:14 +0300

    trycatch block in getCarBrands

--- src/api/getCarBrands.js
+++ src/api/getCarBrands.js
@@ -1,9 +1,14 @@
-import axios from "axios"
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://car-dealer-webserver.onrender.com/api';
 
-export const getCarBrands = async () => { 
-    const response = await axios.get("/carbrands");
+export const getCarBrands = async () => {
+  try {
+    const response = await axios.get('/carbrands');
 
     return response.data;
-}
+  } catch (error) {
+      console.log('Error getting cars:', error);
+      throw error;
+  }
+};
