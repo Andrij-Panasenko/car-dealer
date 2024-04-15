26c89776ac2cf243a9c26d816616e272522583c3
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-15 23:08:14 +0300

    delete temporary fetch() from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { SharedLayout } from './components/SharedLayout/SharedLayout';
 import "./index.css"
 
 export default function App() {
-  // fetch('http://localhost:3000/api/cars').then((i) => console.log(i));
   return (
     <>
       <Header />
