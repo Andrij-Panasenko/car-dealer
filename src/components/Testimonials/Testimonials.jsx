ca5eaeab9840d6b29ce409bbf6ad7089fd1bcf62
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-30 22:39:11 +0300

    styling for Testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,56 @@
+import sprite from '../../assets/sprite.svg';
+import placeholder from '../../assets/avatar-placeholder.png';
+import { SectionTitle } from '../SectionTitle/SectionTitle';
+import {
+  Section,
+  Avatar,
+  Name,
+  Position,
+  Rating,
+  Review,
+  Svg,
+  Testimonial,
+  SectionContainer,
+  SectionWrapp,
+} from './Testimonials.styled';
+
 export const Testimonials = () => {
-    return (
-        <section>Testimonials</section>
-    )
+  return (
+    <Section>
+      <SectionWrapp>
+        <SectionTitle title="Testimonials" />
+        <SectionContainer>
+          <Testimonial>
+            <Avatar src={placeholder} alt="User avatar" />
+            <Svg width="55" height="44">
+              <use xlinkHref={sprite + '#apostroph'}></use>
+            </Svg>
+            <Review>
+              "CarVIP's service is really excellent, I bought a car that I like.
+              The service attitude of the staff here is also great. I will come
+              back to buy!"
+            </Review>
+            <Rating></Rating>
+            <Name>John Doe</Name>
+            <Position>Trans Cosmos Sales Director</Position>
+          </Testimonial>
+
+          <Testimonial>
+            <Avatar src={placeholder} alt="User avatar" />
+            <Svg width="55" height="44">
+              <use xlinkHref={sprite + '#apostroph'}></use>
+            </Svg>
+            <Review>
+              "CarVIP's service is really excellent, I bought a car that I like.
+              The service attitude of the staff here is also great. I will come
+              back to buy!"
+            </Review>
+            <Rating></Rating>
+            <Name>Bom Pak</Name>
+            <Position>Tourist manager</Position>
+          </Testimonial>
+        </SectionContainer>
+      </SectionWrapp>
+    </Section>
+  );
 };
--- src/components/Testimonials/Testimonials.styled.jsx
+++ src/components/Testimonials/Testimonials.styled.jsx
@@ -1,27 +1,82 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Title = styled.h2`
-  width: 175px;
-  height: 42px;
-
-  &::before {
-    
-  }
+export const Section = styled.section`
+  padding-top: 80px;
+  padding-bottom: 80px;
 `;
 
-export const TestimonialsWrapper = styled.div`
+export const SectionWrapp = styled.div`
+  width: 100%;
+  max-width: calc(570px * 2 + 30px);
+  margin: 0 auto;
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  gap: 30px;
+
+  margin-top: 55px;
+`;
+
+export const Testimonial = styled.div`
   border-radius: 5px;
-  background-color: ${p=>p.theme.colors.grey};
+  width: 100%;
+  max-width: 570px;
+
+  padding: 69px 30px 20px 30px;
+
+  position: relative;
+
+  background-color: ${(p) => p.theme.colors.grey};
 `;
 
-export const Avatar = styled.img``;
+export const Avatar = styled.img`
+  width: 110px;
+  height: 110px;
+  border-radius: 100%;
+  border: 5px solid #ffffff;
 
-export const Svg = styled.svg``;
+  position: absolute;
+  left: 30px;
+  top: 0;
+  transform: translateY(-50%);
+`;
 
-export const Review = styled.p``;
+export const Svg = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 8px;
+  transform: translateY(-50%);
+`;
 
-export const Rating = styled.ul``;
+export const Review = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 150%;
 
-export const Name = styled.h3``;
+  margin-bottom: 18px;
 
-export const Position = styled.p``;
+  color: ${(p) => p.theme.colors.text};
+`;
+
+export const Rating = styled.ul`
+  margin-bottom: 16px;
+`;
+
+export const Name = styled.h3`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 150%;
+
+  margin-bottom: 4px;
+
+  color: ${(p) => p.theme.colors.blue};
+`;
+
+export const Position = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+
+  color: ${(p) => p.theme.colors.blue};
+`;
