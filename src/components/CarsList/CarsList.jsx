2943920e404613ba457030a5e4a878a0945dccdb
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-30 22:03:46 +0300

    Update CarsList.jsx
    add component to section

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -1,33 +1,35 @@
 import { useEffect, useState } from 'react';
 import { CardListItem } from './CardListItem/CardListItem';
-import { Divider, List, ListContainer, ListTittle, Section } from './CarsList.styled';
-import { getCarsItem } from "../../api"
+import { Divider, List, ListContainer, Section } from './CarsList.styled';
+import { getCarsItem } from '../../api';
+import { SectionTitle } from '../SectionTitle/SectionTitle';
 
 export const CarsList = () => {
-const [carsItem, setCarsItem] = useState([])
+  const [carsItem, setCarsItem] = useState([]);
 
   useEffect(() => {
     const fetch = async () => {
       const data = await getCarsItem();
       setCarsItem(data);
-      return data
-    }
-    fetch()
-  }, [])
-  
+      return data;
+    };
+    fetch();
+  }, []);
 
   return (
     <>
       <Divider></Divider>
       <Section>
         <ListContainer>
-          <ListTittle>New Car Model</ListTittle>
+          <SectionTitle title="New Car Model" />
           <List>
             {carsItem.map((item) => (
               <CardListItem key={item._id} data={item} />
             ))}
           </List>
-          <button hidden type="button">View all Cars</button>
+          <button hidden type="button">
+            View all Cars
+          </button>
         </ListContainer>
       </Section>
     </>
