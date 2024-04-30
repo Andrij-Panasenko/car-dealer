54b7e31b82f0af24b77af6b193f2c6bf788bfea4
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-30 22:06:17 +0300

    Update CarsList.styled.jsx
    delete old styles

--- src/components/CarsList/CarsList.styled.jsx
+++ src/components/CarsList/CarsList.styled.jsx
@@ -17,27 +17,6 @@ export const Divider = styled.div`
   height: 187px;
 `;
 
-export const ListTittle = styled.h2`
-  display: inline-block;
-  font-weight: 700;
-  font-size: 24px;
-  color: ${(p) => p.theme.colors.blue};
-  position: relative;
-  margin-bottom: 30px;
-  height: 42px;
-
-  &::before {
-    content: '';
-    display: block;
-    width: 100px;
-    height: 3px;
-    background-color: ${(p) => p.theme.colors.orange};
-
-    position: absolute;
-    bottom: 0;
-  }
-`;
-
 export const List = styled.ul`
   /* outline: 1px solid red; */
   display: flex;
