56b0471a6e3f6131f2922504a4e57302553e13a3
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-02 23:21:05 +0300

    slider OurPartners

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+
+import { getOurCustomers } from '../../api';
+import { SectionTitle } from '../SectionTitle/SectionTitle.jsx';
+import { Img, Section, SectionWrapper } from './Partners.styled.jsx';
+
 export const Partners = () => {
-    return (
-        <section>Partners</section>
-    )
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const getPartners = async () => {
+      try {
+        const data = await getOurCustomers();
+        setCustomers(data);
+        return data;
+      } catch (error) {
+        console.log(error, 'Unable to reach customers');
+      }
+    };
+    getPartners();
+  }, []);
+
+  return (
+    <Section>
+      <SectionWrapper>
+        <SectionTitle title="Our Partners" />
+        <Swiper
+          modules={[Navigation, A11y]}
+          navigation
+          spaceBetween={53}
+          slidesPerView={6}
+        >
+          {customers.map((item) => (
+            <SwiperSlide
+              key={item.asset_id}
+            >
+              <Img src={item.secure_url} alt={item.public_id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </SectionWrapper>
+    </Section>
+  );
 };
---
+++ src/components/Partners/Partners.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding-top: 80px;
+  padding-bottom: 80px;
+`;
+
+export const SectionWrapper = styled.div`
+  width: 100%;
+  max-width: 1170px;
+  margin: 0 auto;
+`;
+
+export const Img = styled.img`
+  width: 150px;
+  height: 85px;
+  object-fit: contain;
+`;
+
